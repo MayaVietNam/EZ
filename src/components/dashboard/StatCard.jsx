19263d905524bda6a4f0cf7cca69fe44663b3ef4
MayaVietNam <quanghuy1852000@gmail.com>
2026-03-25 20:12:37 +0700

    Create StatCard.jsx

---
+++ src/components/dashboard/StatCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+export default function StatCard({ title, value, change, icon: Icon, index }) {
+  const isPositive = change >= 0;
+
+  return null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
